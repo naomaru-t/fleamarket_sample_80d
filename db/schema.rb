ddeb3743b8440ab381f4045799744d9615ef545e
naomaru-t <honnkiproguram@gmail.com>
2020-08-11 11:56:58 +0900

    プロフィールテーブルの作成とアソシエーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_06_083721) do
+ActiveRecord::Schema.define(version: 2020_08_11_023349) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -23,6 +23,20 @@ ActiveRecord::Schema.define(version: 2020_08_06_083721) do
     t.string "preparation_days", null: false
     t.integer "price", null: false
     t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name", null: false
+    t.string "family_name", null: false
+    t.string "first_name_kana", null: false
+    t.string "family_name_kana", null: false
+    t.date "birth_year", null: false
+    t.date "birth_month", null: false
+    t.date "birth_day", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -39,4 +53,5 @@ ActiveRecord::Schema.define(version: 2020_08_06_083721) do
   end
 
   add_foreign_key "items", "users"
+  add_foreign_key "profiles", "users"
 end
