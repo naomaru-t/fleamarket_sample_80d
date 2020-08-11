afbaaf7f747470b55e1f196ef8933508fa24c5bc
tatsuma0928 <tatsuma.sano@icloud.com>
2020-08-11 14:28:40 +0900

    カテゴリーテーブルに商品情報を投入

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_08_10_133520) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "ancestry"
