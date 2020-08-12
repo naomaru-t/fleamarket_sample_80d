23ee5db5fe4b5c9c70d903bf257a2f280d521493
naomaru-t <honnkiproguram@gmail.com>
2020-08-12 15:51:04 +0900

    sending登録機能実装

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,15 +3,15 @@ class ProfilesController < ApplicationController
     @profile = Profile.new
   end
   def create
-    @profile = Profile.create(profile_prams)
+    @profile = Profile.create(profile_params)
     if @profile.save
-      redirect_to root_path
+      redirect_to new_sending_path
     else
       render :new
     end
   end
   private
-    def profile_prams
+    def profile_params
       params.require(:profile).permit(:first_name, :family_name, :first_name_kana, :family_name_kana, :birth_date).merge(user_id: current_user.id)
     end
 
