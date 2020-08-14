a61a5bbcb69af6dc188bdda85f750f41a801c695
naomaru-t <honnkiproguram@gmail.com>
2020-08-14 13:02:08 +0900

    ユーザー登録機能バリデーション指定

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,11 @@
 class Profile < ApplicationRecord
   belongs_to :user, optional: true
   validates :first_name, :family_name, :first_name_kana, :family_name_kana, :birth_date,presence: true
+  VALID_FIRSTNAME_REGEX = /\A[一-龥ぁ-ん]/
+  validates :first_name, format: { with: VALID_FIRSTNAME_REGEX }
+  VALID_FAMILYNAME_REGEX = /\A[一-龥]+\z/
+  validates :family_name, format: { with: VALID_FAMILYNAME_REGEX }
+  VALID_KANA_REGEX =/\A[ぁ-んー－]+\z/
+  validates :first_name_kana, format: { with: VALID_KANA_REGEX }
+  validates :family_name_kana, format: { with: VALID_KANA_REGEX }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  VALID_EMAIL_REGEX = /\A\S+@\S+\.\S+\z/
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+
+  validates :password ,presence: true, length: { minimum: 7 }
   validates :nickname ,presence: true
+
   has_one :profile
   has_one :sending
 end
