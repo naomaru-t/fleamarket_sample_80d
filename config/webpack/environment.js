60cd0c02e822b398e1481bb041fd40f9dc00f7d6
naomaru-t <honnkiproguram@gmail.com>
2020-08-17 11:36:08 +0900

    webpack1

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -10,7 +10,7 @@ environment.plugins.prepend('Provide',
 )
 
 
-const webpack = require('webpack')
+
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
