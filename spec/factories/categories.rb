2001c6911b5bdb8908194f48147c9fe3e4999cc3
hiroki0276 <g9daf788e7qcn@gmail.com>
2020-08-19 21:44:43 +0900

    商品の単体テスト

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
-  factory :category do
-    id                {"1"}
-    name              {"???"}
-  end
+  # factory :category do
+  #   id                {"1"}
+  #   name              {"???"}
+  # end
 end
