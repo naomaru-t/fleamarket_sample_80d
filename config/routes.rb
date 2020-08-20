0fd46cc264b426496979d73d990b3ae69947f13a
naomaru-t <honnkiproguram@gmail.com>
2020-08-20 15:51:00 +0900

    マイページのビュー作成

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   end
   root 'items#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show]
   resources :credit_cards, only: [:new, :create, :show, :destroy] do 
     member do 
       get 'buy'
