90464d3671deba28ddb2128ef341dd6ff8e12df7
Shinjiro0126 <mailac63chiokc35@gmail.com>
2020-08-21 18:04:52 +0900

    ユーザー単体テスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname              {"testman"}
+    email                 {"kkk@gmail.com"}
+    password              {"00000000"}
+    password_confirmation {"00000000"}
+    first_name            {"太郎"}
+    last_name             {"山田"}
+    first_name_kana       {"タロウ"}
+    last_name_kana        {"ヤマダ"}
   end
 end
