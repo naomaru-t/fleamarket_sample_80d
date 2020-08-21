beb1f9d792e3c8490c94986354af2f917477cb47
Shinjiro0126 <mailac63chiokc35@gmail.com>
2020-08-21 19:00:40 +0900

    テスト追加

--- spec/models/credit_card_spec.rb
+++ spec/models/credit_card_spec.rb
@@ -2,26 +2,26 @@ require 'rails_helper'
 describe CreditCard do
   describe '#create' do
     it "user_id、customer_id、card_idが存在すれば登録できること" do
-      card = build(:card)
-      expect(card).to be_valid
+      credit_card = FactoryBot.build(:credit_card)
+      expect(credit_card).to be_valid
     end
     it "customer_idがない場合は登録できないこと" do
-      card = build(:card, customer_id: "")
-      card.valid?
-      expect(card.errors[:customer_id]).to include("を入力してください")
+      credit_card = FactoryBot.build(:credit_card, customer_id: "")
+      credit_card.valid?
+      expect(credit_card.errors[:customer_id]).to include("can't be blank")
     end
     it "customer_idが存在すれば登録できること" do
-      card = build(:card, customer_id: "qrltuvwxyz")
-      expect(card).to be_valid
+      credit_card = FactoryBot.build(:credit_card, customer_id: "qrltuvwxyz")
+      expect(credit_card).to be_valid
     end
     it "card_idがない場合は登録できないこと" do
-      card = build(:card, card_id: "")
-      card.valid?
-      expect(card.errors[:card_id]).to include("を入力してください")
+      credit_card = FactoryBot.build(:credit_card, card_id: "")
+      credit_card.valid?
+      expect(credit_card.errors[:card_id]).to include("can't be blank")
     end
     it "card_idが存在すれば登録できること" do
-      card = build(:card, card_id: "qrltuvwxyz")
-      expect(card).to be_valid
+      credit_card = FactoryBot.build(:credit_card, card_id: "qrltuvwxyz")
+      expect(credit_card).to be_valid
     end
   end
 end
