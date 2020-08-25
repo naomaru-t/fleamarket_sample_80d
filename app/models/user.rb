2651860f02e565efcda659b925145fba524d1589
naomaru-t <honnkiproguram@gmail.com>
2020-08-25 17:11:14 +0900

    パスワード文字数変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
 
-  validates :password ,presence: true, length: { minimum: 6 }
+  validates :password ,presence: true, length: { minimum: 7 }
   validates :nickname ,presence: true
 
   has_many :items, dependent: :destroy
